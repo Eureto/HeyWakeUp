37c4c6009b11250b9168ba609966698c1e426a81
Eureto <matek891@gmail.com>
2023-12-03 02:11:06 +0100

    esp32

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -3,8 +3,9 @@
 #define LIGHT_SENSOR_PIN 34 // ESP32 pin GIOP34 (ADC0)
 void setup() {
   Serial.begin(115200);
+  pinMode(26, OUTPUT);
 }
-
+ int brightness{0};
 void loop() {
   // reads the input on analog pin (value between 0 and 4095)
   int analogValue = analogRead(LIGHT_SENSOR_PIN);
@@ -13,17 +14,45 @@ void loop() {
   Serial.print(analogValue);   // the raw analog reading
 
   // We'll have a few threshholds, qualitatively determined
-  if (analogValue < 40) {
-    Serial.println(" => Dark");
-  } else if (analogValue < 800) {
-    Serial.println(" => Dim");
-  } else if (analogValue < 2000) {
-    Serial.println(" => Light");
-  } else if (analogValue < 3200) {
-    Serial.println(" => Bright");
-  } else {
-    Serial.println(" => Very bright");
-  }
+  // if (analogValue < 40) {
+  //   Serial.println(" => Dark");
+  //   analogWrite(26, 255);
+  //   // digitalWrite(26, HIGH);
+  // } else if (analogValue < 800) {
+  //   Serial.println(" => Dim");
+  //   analogWrite(26, 200);
+  //   // digitalWrite(26, HIGH);
+  // } else if (analogValue < 2000) {
+  //   Serial.println(" => Light");
+  //   analogWrite(26, 100);
+  //   // digitalWrite(26, LOW);
+  // } else if (analogValue < 3200) {
+  //   Serial.println(" => Bright");
+  //   // digitalWrite(26, LOW);
+  //   analogWrite(26, 50);
+
+  // } else {
+  //   Serial.println(" => Very bright");
+  //   // digitalWrite(26, LOW);
+  //   analogWrite(26, 0);
+  // }
+ 
+
+//     analogWrite(26, brightness);
+// brightness++;
+// if ( brightness >= 255) {
+//     brightness = 0;}
+
+
+
+ digitalWrite(26, HIGH);
 
   delay(500);
+
+ digitalWrite(26, LOW);
+
+  delay(500);
+
+
+
 }
